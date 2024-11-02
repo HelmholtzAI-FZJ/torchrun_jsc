b2bcbd3d320854342054c7f8a4a7b7a43336a44b
janEbert <janpublicebert@posteo.net>
2024-11-02 20:22:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='torchrun_jsc',
     python_requires='>=3.6,<4',
-    version='0.0.8',
+    version='0.0.9',
     install_requires=[
         'packaging',
         # PyTorch 1.9 introduced the `torchrun` API.
