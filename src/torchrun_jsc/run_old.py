6698e2a59e6d601d9a8c59e0b9f01ed72476be76
janEbert <janpublicebert@posteo.net>
2025-02-14 18:05:03 +0100

    Fix newer PyTorch versions
    More explicitly, PyTorch 2.5 required adaptations.

--- src/torchrun_jsc/run_old.py
+++ src/torchrun_jsc/run_old.py
@@ -33,6 +33,7 @@ import torch
 from torch.distributed.argparse_util import check_env, env
 from torch.distributed.elastic.agent.server import api as sapi
 from torch.distributed.elastic.rendezvous import api as rapi
+from torch.distributed.elastic.rendezvous import dynamic_rendezvous
 from torch.distributed.elastic.utils.distributed import get_free_port
 
 from . import arg_patching
@@ -162,6 +163,31 @@ def build_rendezvous_store_info_build_fn(host):
     return new_build
 
 
+def build_node_desc_generator_generate_fn(host):
+    get_fq_hostname = build_get_fq_hostname_fn(host)
+
+    torch_ver = version.parse(torch.__version__)
+    if torch_ver.major == 2 and torch_ver.minor >= 5 and torch_ver.minor < 6:
+        def new_generate(self, local_addr=None):
+            with self._lock:
+                local_id = self._local_id
+
+                self._local_id += 1
+
+            return dynamic_rendezvous._NodeDesc(
+                local_addr or get_fq_hostname(),
+                os.getpid(),
+                local_id,
+            )
+    else:
+        raise AssertionError(
+            "PyTorch version is too old for applying the "
+            "`_NodeDescGenerator` patch."
+        )
+
+    return new_generate
+
+
 def fix_torch_run(host):
     orig_get_fq_hostname = sapi._get_fq_hostname
     orig_sig = inspect.signature(orig_get_fq_hostname)
@@ -232,6 +258,48 @@ def fix_torch_run_rdvz_store_info(host):
     rapi.RendezvousStoreInfo.build = new_build
 
 
+def fix_torch_run_node_desc_generator(host):
+    torch_ver = version.parse(torch.__version__)
+    # We could actually apply the patch to older versions, too, but
+    # let's not bother with checking the function signature and whatnot
+    # for now.
+    assert (
+        torch_ver.major >= 3
+        or torch_ver.major == 2 and torch_ver.minor >= 5
+    ), (
+        "PyTorch version is too old for applying the "
+        "`_NodeDescGenerator` patch."
+    )
+
+    orig_generate = dynamic_rendezvous._NodeDescGenerator.generate
+    orig_sig = inspect.signature(orig_generate)
+
+    if torch_ver.major == 2 and torch_ver.minor >= 5 and torch_ver.minor < 6:
+        num_orig_parameters = 2
+    else:
+        raise AssertionError(
+            "PyTorch version is too old for applying the "
+            "`_NodeDescGenerator` patch."
+        )
+
+    # Do not replace the function if the number of arguments doesn't
+    # match (we expect a certain number of arguments in the original
+    # version).
+    if host and len(orig_sig.parameters) == num_orig_parameters:
+        new_generate = build_node_desc_generator_generate_fn(host)
+    else:
+        if len(orig_sig.parameters) != num_orig_parameters:
+            warnings.warn(
+                'The function signature of a function that `torchrun_jsc` '
+                'needs to patch has changed; will not apply '
+                '`_NodeDescGenerator` patch. You may be able to ignore '
+                'this warning.'
+            )
+        new_generate = orig_generate
+
+    dynamic_rendezvous._NodeDescGenerator.generate = new_generate
+
+
 def main():
     torch_ver = version.parse(torch.__version__)
     host, conf, is_host = parse_args()
@@ -244,6 +312,13 @@ def main():
             or torch_ver.major == 2 and torch_ver.minor >= 4
     ):
         fix_torch_run_rdvz_store_info(host)
+    # PyTorch 2.5 started to use `_NodeDesc`s for more than just
+    # logging.
+    if (
+            torch_ver.major >= 3
+            or torch_ver.major == 2 and torch_ver.minor >= 5
+    ):
+        fix_torch_run_node_desc_generator(host)
     runpy.run_module('torch.distributed.run', run_name='__main__')
 
 
